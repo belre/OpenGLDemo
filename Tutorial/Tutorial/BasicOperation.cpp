3468f36049177b978dbfecddb1d33072494609b0
Note PC <neopossam@gmail.com>
2022-10-05 15:00:51 +0900

    Interpolation追加、座標系修正

--- Tutorial/Tutorial/BasicOperation.cpp
+++ Tutorial/Tutorial/BasicOperation.cpp
@@ -34,22 +34,29 @@ static const char* vShader = "		      \n\
 																				\n\
 layout (location = 0) in vec3 pos;			\n\
 																				\n\
+out vec4 vCol;													\n\
+																				\n\
 uniform mat4 model;											\n\
 																				\n\
 void main()															\n\
 {																				\n\
 	gl_Position = model * vec4(pos, 1.0);		\n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);			\n\
 }";
 
 // Fragment Shader
 static const char* fShader = "		      \n\
 #version 330														\n\
 																				\n\
+in vec4 vCol;														\n\
+																				\n\
 out vec4 color;													\n\
 																				\n\
 void main()															\n\
 {																				\n\
-	color = vec4(1.0, 0.0, 0.0, 1.0);		  \n\
+	color = vCol;												  \n\
+																				\n\
+																				\n\
 }";
 
 static void AddShader(GLuint the_program, const char* shader_code, GLenum shader_type)
@@ -126,7 +133,7 @@ static void CreateTriangle()
 	GLfloat vertices[] = {
 		-1.0f, -1.0f, 0.0f,
 		1.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 1.0f
+		0.0f, 1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
@@ -247,8 +254,8 @@ int RunBasicOperation()
 
 		glm::mat4 model(1.0f);
 		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-		model = glm::translate(model, glm::vec3(triOffset, 0, 0.0f));
-		model = glm::scale(model, glm::vec3(curSize, 0.4f, 1.0));
+		//model = glm::translate(model, glm::vec3(triOffset, 0, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0));
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
--- Tutorial/Tutorial/Transform.cpp
+++ Tutorial/Tutorial/Transform.cpp
@@ -126,7 +126,7 @@ static void CreateTriangle()
 	GLfloat vertices[] = {
 		-1.0f, -1.0f, 0.0f,
 		1.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 1.0f
+		0.0f, 1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
